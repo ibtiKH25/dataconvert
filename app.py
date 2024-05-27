712b42ea6a7076bf0b883326e2716f14f0a6798b
ibtiKH25 <168609297+ibtiKH25@users.noreply.github.com>
2024-05-27 01:46:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ import io
 
 # MongoDB connection
 client = MongoClient("mongodb://localhost:27017/")
-db1 = client["data_converter"]
-collection = db1["files"]
+db = client["data_converter"]
+collection = db["files"]
 
 # URL du fichier modèle sur GitHub
 model_url = 'https://github.com/ibtiKH25/dataconvert/raw/main/TrainingModel.pt'
