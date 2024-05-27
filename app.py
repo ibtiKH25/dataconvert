8cc0fccdeef798431f2476e433ec6a17691990b0
ibtiKH25 <168609297+ibtiKH25@users.noreply.github.com>
2024-05-27 04:43:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -143,9 +143,9 @@ def main():
     add_custom_css()
     
     st.sidebar.title("Navigation")
-    page = st.sidebar.selectbox("Select a page", ["Upload and Process", "View Saved Data"])
+    page = st.sidebar.selectbox("Select a page", ["Convert Data", "Historique"])
 
-    if page == "Upload and Process":
+    if page == "Convert Data":
         st.title('Data Converter LEONI \n Convert Technical Drawings with Accuracy and Ease')
         uploaded_file = st.file_uploader("Choose an image to analyze...", type=["jpg", "png", "jpeg"])
         
@@ -234,8 +234,8 @@ def main():
             else:
                 st.write("No detections or incorrect result format.")
 
-    elif page == "View Saved Data":
-        st.title('View Saved Data')
+    elif page == "Historique":
+        st.title('Historique')
 
         # Add a search box
         search_query = st.text_input("Search by image name:")
